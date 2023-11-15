3943d3f94ab72312dd0cfeb686213ff82954a67f
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-11-15 05:21:19 +0500

    dashboard

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// aside
+
+const sidebar = document.querySelector("aside");
+const sidebarToggle = document.querySelector(".hamburger");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+if (innerWidth <= 1000) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
+
+// dark-light
+
+const body = document.body;
+const modeToggle = document.querySelector(".switch");
+
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
